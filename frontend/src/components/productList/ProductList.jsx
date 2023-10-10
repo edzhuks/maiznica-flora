cb83acb25787f81976917f7827e866c60a701bc1
edgars <edzhuks@gmail.com>
2023-10-10 16:42:37 +0300

    responsive cart

--- frontend/src/components/productList/ProductList.jsx
+++ frontend/src/components/productList/ProductList.jsx
@@ -1,18 +1,6 @@
+import { EmptyProductItem, ProductRow, Spacer } from '../styled/base'
 import ProductListItem from './ProductListItem'
 import styled from 'styled-components'
-
-const ProductRow = styled.div`
-  display: flex;
-  align-items: stretch;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-  gap: 30px;
-`
-
-const EmptyProductItem = styled.div`
-  width: calc(100% / 4 - 23px);
-  height: 0;
-`
 
 const ProductList = ({ products }) => {
   return (
@@ -24,9 +12,9 @@ const ProductList = ({ products }) => {
         />
       ))}
 
-      <EmptyProductItem />
-      <EmptyProductItem />
-      <EmptyProductItem />
+      <Spacer />
+      <Spacer />
+      <Spacer />
     </ProductRow>
   )
 }
