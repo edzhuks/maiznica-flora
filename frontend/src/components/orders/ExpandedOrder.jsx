b24d436174333c7f43d8627c0fb76fbd3afa5e6f
edgars <edzhuks@gmail.com>
2023-12-08 15:12:01 +0200

    fix another problem with emtpy business comments

--- frontend/src/components/orders/ExpandedOrder.jsx
+++ frontend/src/components/orders/ExpandedOrder.jsx
@@ -300,27 +300,30 @@ const ReadyForPickupModal = ({ visible, close, order, submit }) => {
           <h3 className="card-heading">{lang.deliveryComments}</h3>
           <p className="card-text wrap-n">{order.deliveryComments}</p>
           <div className="card-divider" />
-          {order.businessComments && (
-            <>
-              <h3 className="card-heading">{lang.businessComments}</h3>
-              {order.businessComments.name ? (
-                <>
-                  <p className="card-text wrap-n">
-                    {order.businessComments.name}
-                  </p>
-                  <p className="card-text wrap-n">
-                    {order.businessComments.address}
-                  </p>
-                  <p className="card-text wrap-n">
-                    {order.businessComments.regNo}
-                  </p>
-                </>
-              ) : (
-                <p>{order.businessComments}</p>
-              )}
-              <div className="card-divider" />
-            </>
-          )}
+          {order.businessComments &&
+            (order.businessComments.length > 0 ||
+              (order.businessComments.name &&
+                order.businessComments.name.length > 0)) && (
+              <>
+                <h3 className="card-heading">{lang.businessComments}</h3>
+                {order.businessComments.name ? (
+                  <>
+                    <p className="card-text wrap-n">
+                      {order.businessComments.name}
+                    </p>
+                    <p className="card-text wrap-n">
+                      {order.businessComments.address}
+                    </p>
+                    <p className="card-text wrap-n">
+                      {order.businessComments.regNo}
+                    </p>
+                  </>
+                ) : (
+                  <p>{order.businessComments}</p>
+                )}
+                <div className="card-divider" />
+              </>
+            )}
           <h3 className="card-heading">{lang.generalComments}</h3>
           <p className="card-text wrap-n">{order.generalComments}</p>
         </div>
